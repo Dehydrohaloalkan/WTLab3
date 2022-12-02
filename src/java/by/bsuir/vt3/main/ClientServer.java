ecfdf1aad26a6ccca860bcbaddc37276a31b717c
alexb <alexbondar03@gmail.com>
2022-12-02 21:37:05 +0300

    Add main

--- src/java/by/bsuir/vt3/main/ClientServer.java
+++ src/java/by/bsuir/vt3/main/ClientServer.java
@@ -1,7 +1,14 @@
 package java.by.bsuir.vt3.main;
 
+import java.by.bsuir.vt3.client.main.Client;
+import java.by.bsuir.vt3.server.main.Server;
+
 public class ClientServer {
     public static void main(String[] args){
+        Server server = new Server();
+        Client client = new Client();
 
+        server.run();
+        client.run();
     }
 }
